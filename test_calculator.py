f8495392a1ca15824baa6989f4b6893a9e3091b2
Isabelle Bretl <bretli@oregonstate.edu>
2021-06-01 11:37:13 -0700

    added tests

---
+++ test_calculator.py
@@ -0,0 +1,12 @@
+"""
+Tests for calculator app
+"""
+import calculator
+
+
+class TestCalculatorApp:
+    def test_add(self):
+        assert 5 == calculator.add(3,2)
+
+    def test_subract(self):
+        assert 5 == calculator.subract(10,5)
